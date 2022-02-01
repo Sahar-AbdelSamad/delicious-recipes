d464d9c5cf7c96899e6dcca3dbc1e7b3d83f42f8
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-02-01 21:07:01 +0200

    Display number of likes on the homepage

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import listItems from './listItems.js';
+import { getAPI } from './involvementAPI.js';
 import './style.css';
 
 window.onload = () => {
   listItems();
+  getAPI().then((response) => {
+    const like = Array.from(document.querySelectorAll('.likes'));
+    for (let i = 0; i < like.length; i += 1) {
+      response.forEach((item) => {
+        if (item.item_id === like[i].id) {
+          const small = document.createElement('small');
+          small.className = ('small');
+          small.textContent = `${item.likes} likes`;
+          like[i].appendChild(small);
+        }
+      });
+    }
+  });
 };
--- src/listItems.js
+++ src/listItems.js
@@ -6,7 +6,6 @@ const listItems = async () => {
   food.forEach((element) => {
     const li = document.createElement('li');
     li.className = ('recipe-item');
-    li.id = element.idMeal;
 
     const img = document.createElement('img');
     img.className = ('food-img');
@@ -19,12 +18,11 @@ const listItems = async () => {
 
     const div = document.createElement('div');
     div.className = ('likes');
+    div.id = element.idMeal;
 
     const heart = document.createElement('i');
     heart.classList.add('far', 'fa-heart');
     heart.id = element.idMeal;
-    const small = document.createElement('small');
-    small.textContent = '5 likes';
 
     const btn = document.createElement('button');
     btn.type = 'button';
@@ -33,7 +31,6 @@ const listItems = async () => {
     btn.id = element.idMeal;
 
     div.appendChild(heart);
-    div.appendChild(small);
     p.appendChild(div);
     li.appendChild(img);
     li.appendChild(p);
