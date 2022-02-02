15eb8ef58fedcb4ed7df3dadee3cc9ec9b20105d
danielufeli <danielufeli@yahoo.com>
2022-02-02 23:31:31 +0100

    Add form to modal

--- src/modules/involvementAPI.js
+++ src/modules/involvementAPI.js
@@ -19,3 +19,14 @@ export const getAPI = async () => {
   const data = await response.json();
   return data;
 };
+
+export const postCommentAPI = async (body) => {
+  const response = await fetch(`${url}comments/`, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  return response.text();
+};
